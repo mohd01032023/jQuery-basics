4d284ea2347bc2e9d449869002df648842cd1d63
shuaibcedcoss <126545931+mohd01032023@users.noreply.github.com>
2023-04-11 18:04:42 +0530

    Update dynamic-list.js

--- jQuery-basics/src/JS/dynamic-list.js
+++ jQuery-basics/src/JS/dynamic-list.js
@@ -1,10 +1,6 @@
 $(function(){
     $(document).on('click', 'button', function(){
-        let newItem = $('input').val().trim(); // Remove end whitespaces
-        if(newItem != "" && /^[a-zA-Z0-9\s]+$/.test(newItem)){
-            $(this).parent().children('ul').append(`<li>${newItem}<button type="button">+</button> <ul></ul> </li>`)
-        } else {
-            alert("Please enter a valid category/subcategory")
-        }
+        let toAdd = "<ul>" + $(this).parent().parent().html() + "</ul>";
+        $(this).parent().parent().parent().append(toAdd);
     })
 })
